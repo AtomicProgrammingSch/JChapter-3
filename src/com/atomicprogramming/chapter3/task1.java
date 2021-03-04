659442e5dcd07b41deae7c6a611e611157af068f
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-04 03:05:05 +0000

    Finished task1

--- src/com/atomicprogramming/chapter3/task1.java
+++ src/com/atomicprogramming/chapter3/task1.java
@@ -1,7 +1,40 @@
 package com.atomicprogramming.chapter3;
 
-public class task1 {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class task1 {
+    public static boolean another() {
+        Scanner scanner = new Scanner(System.in);
+        String reply;
+        while (true) {
+            System.out.println("Do you have another favourite quotation? (y/n)");
+            reply = scanner.nextLine();
+            if (reply.equals("y")) {
+                return true;
+            } else if (reply.equals("n")) {
+                return false;
+            } else {
+                System.out.println("Invalid response!");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<String> quotations = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String quote;
+        boolean another;
+        do {
+            System.out.println("Please enter one of your favourite quotations:");
+            quote = scanner.nextLine();
+            quotations.add(quote);
+            another = another();
+        } while (another);
+        String quotes = "Your favourite quotations are:\n";
+        for (String quotation : quotations) {
+            assert false;
+            quotes += quotation + "\n";
+        }
+        System.out.println(quotes);
     }
 }
