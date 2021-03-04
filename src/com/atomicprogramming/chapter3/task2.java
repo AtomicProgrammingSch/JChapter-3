af9c126866afe669e96b5c947fc2e78a92e9e9b1
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-04 21:52:40 +0000

    Finished task2

---
+++ src/com/atomicprogramming/chapter3/task2.java
@@ -0,0 +1,8 @@
+package com.atomicprogramming.chapter3;
+
+public class task2 {
+    public static void main(String[] args) {
+        String name = "George";
+        System.out.println("My name is " + name);
+    }
+}
