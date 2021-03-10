e24e51322655c2a91b0458b2d0127ec8427272c9
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-10 11:04:05 +0000

    Finished task-8 alternative

---
+++ src/com/atomicprogramming/chapter3/task8Alternative.java
@@ -0,0 +1,11 @@
+package com.atomicprogramming.chapter3;
+
+public class task8Alternative {
+    public static void main(String[] args) {
+        // Code given in the question:
+        String title = "GCSE Python for Key Stage 4";
+        // Response:
+        String substring = title.split(" ")[3];
+        System.out.println("The substring is: " + substring);
+    }
+}
