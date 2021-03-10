74c43aa5478fbd25e9cb5010c8f7221172903a02
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-10 12:46:45 +0000

    Finished task-9

---
+++ src/com/atomicprogramming/chapter3/task9.java
@@ -0,0 +1,8 @@
+package com.atomicprogramming.chapter3;
+
+public class task9 {
+    public static void main(String[] args) {
+        String name = "Geoffrey";
+        System.out.println("Hello, " + name + " Java is fun");
+    }
+}
